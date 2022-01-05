84c3197af03bc8a97fd256038902fd55e1ff914d
neacsuTeodora <georgiana.neacsu99@stud.acs.upb.ro>
2022-01-05 17:01:23 +0200

    Added authentication

---
+++ party-games-app/src/components/Auth/SignUp/SignUp.js
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../../../contexts/contexts";
+import { Link, useHistory } from "react-router-dom";
+import { database } from "../../../firebase";
+import './SignUp.css';
+
+export default function SignUp() {
+    const nameRef = useRef();
+    const groupRef = useRef();
+    const userRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const { signup } = useAuth();
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        const userData = {
+            name: nameRef.current.value,
+            email: emailRef.current.value
+        }
+        database.ref('users').push(userData);
+    
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+          return setError("Passwords do not match");
+        }
+    
+        try {
+          setError("");
+          setLoading(true);
+          await signup(emailRef.current.value, passwordRef.current.value);
+          history.push("/");
+        } catch(error) {
+            setError(error.message);
+        }
+    
+        setLoading(false);
+    }
+
+    return (
+        <>
+            <Card id='card-container-signup'>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Sign Up</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" ref={nameRef} required />
+                        </Form.Group>
+
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
+                        </Form.Group>
+
+                        <Form.Group id="password-confirm" style={{ marginBottom: "20px" }}>
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} required />
+                        </Form.Group>
+
+                        <Button disabled={loading} className="w-100 auth-button" type="submit">
+                            Sign Up
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            
+            <div className="w-100 text-center mt-2" id='login-check'>
+                Already have an account? <Link to="/login">Log In</Link>
+            </div>
+        </>
+    )
+}
---
+++ party-games-app/src/utils/utils.js
@@ -0,0 +1,42 @@
+import { database } from "../firebase";
+
+export function CheckIfUserIsStudent(email: string) {
+    return email.endsWith("@stud.acs.upb.ro") ? true : false;
+}
+
+export function CheckIfUserIsAdmin(email: string) {
+    return email === ("admin@cs.pub.ro") ? true : false;
+}
+
+export function GetFirebaseKeyStudent(email: string) {
+    const Refs = database.ref('users');
+    let entry = "";
+
+    Refs.on('value', snapshot => {
+      snapshot.forEach(childSnapshot => {
+          const childData = childSnapshot.val();
+          if(childData.email === email) {
+            entry = childSnapshot.key;
+          }
+      });
+    });
+
+    return entry;
+}
+
+export function CheckIfUserIsActive(email: string, refs: string) {
+
+    const Refs = database.ref(refs);
+    let check = false;
+    Refs.on('value', snapshot => {  
+        
+      snapshot.forEach(childSnapshot => {
+          const childData = childSnapshot.val();
+          if(childData.isActive === true && childData.email === email) {
+              check = true;
+          }
+      });
+    });
+
+    return check;
+}
