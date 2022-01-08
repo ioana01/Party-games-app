9e0a43d1be77930a8c3f44e7d45340139e480625
neacsuTeodora <georgiana.neacsu99@stud.acs.upb.ro>
2022-01-09 00:34:07 +0200

    Exit guest + join room

--- party-games-app/src/components/extended-room-info/extended-room-info.js
+++ party-games-app/src/components/extended-room-info/extended-room-info.js
@@ -1,14 +1,66 @@
 import React, {Component} from 'react';
-import { database } from "../../firebase";
+import { auth, database } from "../../firebase";
 import { Link } from "react-router-dom";
-import './extended-room-info.css'
+import './extended-room-info.css';
+import MyPopUp from "../PopUp/PopUp";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { Card, Form, Button, Alert } from "react-bootstrap";
+
 class ExtendedInfo extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             roomId: `-${this.props.match.params.id}`,
+            isOpen: false,
             roomInfo: {}
+        }
+    }
+  
+    togglePopupOpen = (e) => {
+        if (this.state.isOpen == false){
+            this.setState({ isOpen: true });
+        }
+    }
+
+    handleClose = (e) => {
+        this.setState({ isOpen: false });
+    }
+  
+    joinRoom = (e) => {
+        let playersList = this.state.roomInfo.players;
+        playersList.push({'name': auth.currentUser.email, 'score': 0});
+        console.log(playersList);
+        database.ref('/rooms').child(this.state.roomId).update({'players': playersList});
+        database.ref('/rooms').child(this.state.roomId).update({'current_users_number': this.state.roomInfo.current_users_number + 1});
+        this.componentDidMount();
+    }
+
+    exitRoom = (e) => {
+        let playersList = this.state.roomInfo.players;
+        for( var i = 0; i < playersList.length; i++){ 
+    
+            if ( playersList[i].name === auth.currentUser.email) { 
+        
+                playersList.splice(i, 1); 
+            }
+        
+        }
+        console.log(playersList);
+        database.ref('/rooms').child(this.state.roomId).update({'players': playersList});
+        database.ref('/rooms').child(this.state.roomId).update({'current_users_number': this.state.roomInfo.current_users_number - 1});
+        this.componentDidMount();
+    }
+    checkIfInRoom() {
+        let playersList = [];
+        if(this.state.roomInfo && this.state.roomInfo.players) {
+            const players = this.state.roomInfo.players;
+            playersList = players.map((player) => {return player.name})
+            if (playersList.indexOf(auth.currentUser.email) > -1){
+                return true;
+            }
+            return false;
         }
     }
 
@@ -28,6 +80,7 @@ class ExtendedInfo extends Component {
 
             this.setState({ roomInfo : room });
         });
+
     }
 
     render() {
@@ -55,7 +108,50 @@ class ExtendedInfo extends Component {
                 <p className="card-text text-secondary"> Audience score: { this.state.roomInfo.audience_score} points</p>
 
                 <div className='join-game-btn-container'>
-                    <Link className="btn btn-outline-success join-game-btn" to={{pathname: `/game/`}}> Join game </Link>
+                    {(() => {
+                        if (this.checkIfInRoom()){
+                            if (this.state.roomInfo.admin_name == auth.currentUser.email){
+                                return (
+                                    <div>
+                                        <Button className="btn btn-outline-success join-game-btn" onClick={(e) => this.exitRoom(e)}> Exit room </Button>
+                                        <Link className="btn btn-outline-success join-game-btn" to={{pathname: `/game/`}}> Start game </Link>
+                                    </div>
+                                )
+                            } else {
+                                return (
+                                    <Button className="btn btn-outline-success join-game-btn" onClick={(e) => this.exitRoom(e)}> Exit room </Button>
+                                )
+                            }
+                        } else{
+                            if (this.state.roomInfo.current_users_number >= this.state.roomInfo.max_users_number){
+                                return (
+                                    <Popup trigger={<Button className="btn btn-outline-success join-game-btn"> Join room </Button>} position="right center">
+                                        <div>Room is already at maximum capacity of users!</div>
+                                    </Popup>
+                                )
+                            } else {
+                                if (this.state.roomInfo["type"] == "private") {
+                                    return (
+                                        <div>
+                                            <Button className="btn btn-outline-success join-game-btn" onClick={(e) => this.togglePopupOpen(e)}>
+                                            <MyPopUp 
+                                                trigger = {this.state.isOpen}
+                                                handleClose = {this.handleClose}
+                                                button = "Join room"
+                                                roomId = {this.state.roomId} 
+                                                joinRoom = {this.joinRoom}/>
+                                            Join Room
+                                            </Button>
+                                        </div>
+                                    )
+                                } else {
+                                    return (
+                                        <Button className="btn btn-outline-success join-game-btn" onClick={(e) => this.joinRoom(e)}> Join room </Button>
+                                    )
+                                }
+                            }
+                        }    
+                        })()}
                 </div>
             </div>
         )
--- party-games-app/src/components/navbar/navbar.js
+++ party-games-app/src/components/navbar/navbar.js
@@ -2,25 +2,43 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navbar, Nav } from 'react-bootstrap';
 import { useAuth } from "../../contexts/contexts";
-import { auth } from "../../firebase";
+import { auth, database } from "../../firebase";
 import './navbar.css';
+import { CheckIfUserIsGuest, verifyGuest } from '../../utils/utils';
 
 export default function BootstrapNavbar() {
     const { logout } = useAuth();
     const [error, setError] = useState("")
     let email = undefined;
-
     if(auth.currentUser != null) {
         email = auth.currentUser.email
     }
 
     async function handleLogout() {
         setError("")
-
+        console.log("logout")
+        console.log(auth.currentUser.email)
         try {
-            // TODO: daca e guest, se sterge intrarea din tabela `guests`
-            // TODO: si stergere din authentication (firebase -> Teodora)
-            await logout();
+            let guest = false;
+            let entryKey = "";
+            await database.ref('guests').once('value', snapshot => {
+                snapshot.forEach(childSnapshot => {
+                    const childData = childSnapshot.val();
+                    if (childData['email'] === email) {
+                        console.log("guest useeeer");
+                        guest = true;
+                        entryKey = childSnapshot.key;
+                    }
+                });
+            });
+            if (guest){
+                await auth.currentUser.delete().then().catch(function (error) {
+                    console.error({error})
+                });
+                await database.ref('guests/').child(entryKey).remove();
+            } else {
+                await logout();
+            }
         } catch {
             setError("Failed to log out");
         }
@@ -36,7 +54,7 @@ export default function BootstrapNavbar() {
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="ml-auto">
-                                        <Nav.Link href="/login" onClick={handleLogout}>Log Out</Nav.Link>
+                                        <Nav.Link onClick={handleLogout}>Log Out</Nav.Link>
                                     </Nav>
                                 </Navbar.Collapse>
                         </Navbar>
