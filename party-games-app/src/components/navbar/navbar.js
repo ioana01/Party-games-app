e6adacbc55b0fff438c7f104d9c9cbcd3c558614
Mihai Catalin Valentin <catalin.mihai2199@gmail.com>
2022-01-11 09:47:00 +0200

    fix logout

--- party-games-app/src/components/navbar/navbar.js
+++ party-games-app/src/components/navbar/navbar.js
@@ -38,18 +38,21 @@ export default function BootstrapNavbar() {
                     snapshot.forEach(childSnapshot => {
                         const childData = childSnapshot.val();
                         let playersList = childData['players'];
-                        const playerLen = playersList.length;
+                        
+                        if(playersList) {
+                            const playerLen = playersList.length;
 
-                        for(let i = 0; i < playersList.length; i++){ 
-                            if ( playersList[i].name === auth.currentUser.email) { 
-                                playersList.splice(i, 1); 
+                            for(let i = 0; i < playersList.length; i++){ 
+                                if ( playersList[i].name === auth.currentUser.email) { 
+                                    playersList.splice(i, 1); 
+                                }
+                            
                             }
-                        
-                        }
-                        
-                        if ( playerLen > playersList.length) { 
-                            database.ref('/rooms').child(childSnapshot.key).update({'players': playersList});
-                            database.ref('/rooms').child(childSnapshot.key).update({'current_users_number': childData["current_users_number"] - 1});
+                            
+                            if ( playerLen > playersList.length) { 
+                                database.ref('/rooms').child(childSnapshot.key).update({'players': playersList});
+                                database.ref('/rooms').child(childSnapshot.key).update({'current_users_number': childData["current_users_number"] - 1});
+                            }
                         }
                     });
                 });
@@ -73,13 +76,13 @@ export default function BootstrapNavbar() {
                 <div className="col-md-12" id='navbar-container'>
                     <Router>
                         <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                            <Navbar.Brand href="#home">Party games app</Navbar.Brand>
+                            <Navbar.Brand to="#home">Party games app</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="ml-auto">
                                         <Nav.Link href="/" >Dashboard</Nav.Link>
                                         <Nav.Link href="/newRoom" >Create new room</Nav.Link>
-                                        <Nav.Link href="/login" onClick={handleLogout}>Log Out</Nav.Link>
+                                        <Nav.Link onClick={handleLogout}>Log Out</Nav.Link>
                                     </Nav>
                                 </Navbar.Collapse>
                         </Navbar>
